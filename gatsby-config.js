bbfb208e37055daa2aa99ae1d4b0155657caa523
Vrushali Bhosale <vrushali.bhosale@qed42.com>
2020-05-19 12:26:45 +0530

    added gatsby-plugin-sw

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-sw',
+      // options: {
+      //   swPath: 'src/utils/my-service-worker.js', // Default to 'src/sw.js'
+      // },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
