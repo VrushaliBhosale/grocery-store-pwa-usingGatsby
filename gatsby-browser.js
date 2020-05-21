6c77fb93da0eda5f78e021a5644c1c5cb1d61f43
Vrushali Bhosale <vrushali.bhosale@qed42.com>
2020-05-21 11:09:29 +0530

    changes in gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,4 +5,33 @@
  */
 
 // You can delete this file if you're not using it
-exports.registerServiceWorker = () => true;
+// exports.registerServiceWorker = () => true;
+
+// export const onServiceWorkerUpdateFound = () => {
+  const showNotification = () => {
+    Notification.requestPermission(result => {
+        if (result === 'granted') {
+            navigator.serviceWorker.ready.then(registration => {
+                registration.showNotification('Update', {
+                    body: 'New content is available!',
+                    icon: 'images/example.png',
+                    vibrate: [200, 100, 200, 100, 200, 100, 400],
+                    tag: 'request',
+                    actions: [ // you can customize these actions as you like
+                      {
+                        action: 'explore', title: 'Explore this new world',
+                        // icon: 'images/checkmark.png'
+                      },
+                      {
+                          action: 'close', title: 'Close',
+                          // icon: 'images/xmark.png'
+                      },
+                    ]
+                })
+            })
+        }
+    })
+  }
+
+  showNotification()
+// }
