5d2ca8222ef00605d4bdaf59ac63cbb46f1f22c8
Vrushali Bhosale <vrushali.bhosale@qed42.com>
2020-05-21 11:23:39 +0530

    added append script

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,34 +4,34 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
-// exports.registerServiceWorker = () => true;
+// You can delete this file if you're not using its
+exports.registerServiceWorker = () => true;
 
 // export const onServiceWorkerUpdateFound = () => {
-  const showNotification = () => {
-    Notification.requestPermission(result => {
-        if (result === 'granted') {
-            navigator.serviceWorker.ready.then(registration => {
-                registration.showNotification('Update', {
-                    body: 'New content is available!',
-                    icon: 'images/example.png',
-                    vibrate: [200, 100, 200, 100, 200, 100, 400],
-                    tag: 'request',
-                    actions: [ // you can customize these actions as you like
-                      {
-                        action: 'explore', title: 'Explore this new world',
-                        // icon: 'images/checkmark.png'
-                      },
-                      {
-                          action: 'close', title: 'Close',
-                          // icon: 'images/xmark.png'
-                      },
-                    ]
-                })
-            })
-        }
-    })
-  }
+  // const showNotification = () => {
+  //   Notification.requestPermission(result => {
+  //       if (result === 'granted') {
+  //           navigator.serviceWorker.ready.then(registration => {
+  //               registration.showNotification('Update', {
+  //                   body: 'New content is available!',
+  //                   icon: 'images/example.png',
+  //                   vibrate: [200, 100, 200, 100, 200, 100, 400],
+  //                   tag: 'request',
+  //                   actions: [ // you can customize these actions as you like
+  //                     {
+  //                       action: 'explore', title: 'Explore this new world',
+  //                       // icon: 'images/checkmark.png'
+  //                     },
+  //                     {
+  //                         action: 'close', title: 'Close',
+  //                         // icon: 'images/xmark.png'
+  //                     },
+  //                   ]
+  //               })
+  //           })
+  //       }
+  //   })
+  // }
 
-  showNotification()
+  // showNotification()
 // }
